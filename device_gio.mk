7de2fe3cc077ab7c1072fb4cc923d2319e87094a
Giovanni Santini <itachi.sama.amaterasu@gmail.com>
2013-06-17 22:47:20 +0200

    We're on CM9

--- device_gio.mk
+++ device_gio.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, vendor/samsung/gio/vendor_blobs.mk)
 
 # MMB Vendor
-$(call inherit-product, vendor/mmb/vendor_blobs.mk)
+$(call inherit-product, vendor/mmb/vendor_blobs_cm.mk)
 
 # Including GApps
 $(call inherit-product, vendor/google/tiny.mk)
